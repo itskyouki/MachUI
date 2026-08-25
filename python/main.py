134fb1253b37b7a8e4cab0deede544910e412410
itskyouki <itzkyouki@gmail.com>
2026-08-25 15:13:20 +1000

    added weather.py into the main.py loop with a 2 hour polling rate

--- python/main.py
+++ python/main.py
@@ -1,15 +1,19 @@
+import time
 from modules import clock
 from modules import battery
-import time
+from modules import weather
 
 # start up stuff
 clock_info = clock.get_time()
 battery_info = battery.get_battery()
+weather_info = weather.get_weather()
 
 # loop stuff
 current_time = time.time()
 lu_clock = current_time
 lu_battery = current_time
+lu_weather = current_time
+
 #main data loop
 while True:
     current_time = time.time()
@@ -19,4 +23,7 @@ while True:
     if current_time - lu_battery >= 4:
             battery_info = battery.get_battery()
             lu_battery = current_time
+    if current_time - lu_weather >= 7200:
+        weather_info = weather.get_weather()
+        lu_weather = current_time
     time.sleep(0.1)
