6078c3ab992a1451619544839ff3542d7c836689
itskyouki <itzkyouki@gmail.com>
2026-08-26 03:34:07 +1000

    made the weather module use the dumped json from location and made the location update per hour

--- python/main.py
+++ python/main.py
@@ -26,14 +26,14 @@ while True:
         lu_clock = current_time
 
     if current_time - lu_battery >= 4:
-            battery.get_battery()
-            lu_battery = current_time
+        battery.get_battery()
+        lu_battery = current_time
 
     if current_time - lu_weather >= 7200:
         weather.get_weather()
         lu_weather = current_time
 
     if current_time - lu_location >= 3600:
-            autolocation.get_location()
-            lu_location = current_time
+        autolocation.get_location()
+        lu_location = current_time
     time.sleep(0.1)
--- python/modules/weather.py
+++ python/modules/weather.py
@@ -5,21 +5,24 @@ import json
 def get_weather():
     # data path
     weather_file = Path(__file__).parent.parent.parent / "data" / "weather.json"
-# lat and long (replace with auto location)
-    latitude =  78
-    longitude = 78
+    with open("data/location.json", "r") as file:
+        location_info = json.load(file)
+        city = location_info["city"]
+        country = location_info["country"]
+        timezone = location_info["timezone"]
+        latitude = location_info["latitude"]
+        longitude = location_info["longitude"]
     url = (
         "https://api.open-meteo.com/v1/forecast?"
         f"latitude={latitude}&longitude={longitude}"
         "&daily=sunrise,sunset,uv_index_max,precipitation_probability_max"
         "&hourly=temperature_2m,precipitation_probability,apparent_temperature,wind_speed_10m,weather_code"
-        "&timezone=Australia%2FSydney"
+        f"&timezone={timezone}"
 )
     response = requests.get(url)
     weather_info = response.json()
-    print(weather_info)
     with open(weather_file, "w") as weatherfile:
         json.dump(weather_info, weatherfile)
     return weather_info
 
-get_weather
+
